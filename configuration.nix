c621bcce9096f6a4d5240ad19cdd2d047f523350
Kai Breucker <kai.breucker@gmail.com>
2023-10-21 07:24:56 +0200

    Added the necessary nixosConfigurations attribute to configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -4,57 +4,64 @@
 { config, lib, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
-
   # Omit the previous configuration...
 
   # Enable Flakes and the new command-line tool
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  environment.systemPackages = with pkgs; [
-    # Flakes use Git to pull dependencies from data sources 
-    coreutils
-    git
-    vim
-    wget
-    curl
-  ];
-  # Set default editor to vim
-  environment.variables.EDITOR = "vim";
+  # Define a nixosConfigurations attribute under nixos
+  nixosConfigurations.mySystem = {
+    system = "x86_64-linux";
+    # Add the following line to fix the error
+    config.system.build = "x86_64-linux";
 
-  # Create a user.
-  users.users.johndoe = {
-    isNormalUser = true;
-    home = "/home/kai";
-    description = "Kai";
-  };
+    imports =
+      [ # Include the results of the hardware scan.
+        ./hardware-configuration.nix
+      ];
 
-  # Use Home Manager to manage the user's dotfiles.
-  imports = [
-    <home-manager/nixos>
-  ];
+    environment.systemPackages = with pkgs; [
+      # Flakes use Git to pull dependencies from data sources 
+      coreutils
+      git
+      vim
+      wget
+      curl
+    ];
+    # Set default editor to vim
+    environment.variables.EDITOR = "vim";
 
-  home.file.".config/nixpkgs/config.nix" = {
-    source = ./home.nix;
-    text = ''
-      { home.username }:
-      {
-        programs.home-manager.enable = true;
-      };
-    '';
-  };
-
-  # Enable the OpenSSH daemon.
-  services.openssh = {
-    enable = true;
-    settings = {
-      X11Forwarding = true;
-      PermitRootLogin = "no"; # disable root login
-      PasswordAuthentication = false; # disable password login
+    # Create a user.
+    users.users.kai = {
+      isNormalUser = true;
+      home = "/home/kai";
+      description = "Kai";
     };
-    openFirewall = true;
+
+    # Use Home Manager to manage the user's dotfiles.
+    imports = [
+      <home-manager/nixos>
+    ];
+
+    home.file.".config/nixpkgs/config.nix" = {
+      source = ./home.nix;
+      text = ''
+        { home.username }:
+        {
+          programs.home-manager.enable = true;
+        };
+      '';
+    };
+
+    # Enable the OpenSSH daemon.
+    services.openssh = {
+      enable = true;
+      settings = {
+        X11Forwarding = true;
+        PermitRootLogin = "no"; # disable root login
+        PasswordAuthentication = false; # disable password login
+      };
+      openFirewall = true;
+    };
   };
 }
