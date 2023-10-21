eeea5199424fc81b2dfe5717ce4128227e0735e3
Kai Breucker <kai.breucker@gmail.com>
2023-10-21 07:34:00 +0200

    Fixed the structure of configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -9,59 +9,60 @@
   # Enable Flakes and the new command-line tool
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  # Define a nixosConfigurations attribute under nixos
-  nixosConfigurations.mySystem = {
-    system = "x86_64-linux";
-    # Add the following line to fix the error
-    config.system.build = "x86_64-linux";
+  nixos = {
+    nixosConfigurations.mySystem = {
+      system = "x86_64-linux";
+      # Add the following line to fix the error
+      config.system.build = "x86_64-linux";
 
-    imports =
-      [ # Include the results of the hardware scan.
-        ./hardware-configuration.nix
+      imports =
+        [ # Include the results of the hardware scan.
+          ./hardware-configuration.nix
+        ];
+
+      environment.systemPackages = with pkgs; [
+        # Flakes use Git to pull dependencies from data sources 
+        coreutils
+        git
+        vim
+        wget
+        curl
+      ];
+      # Set default editor to vim
+      environment.variables.EDITOR = "vim";
+
+      # Create a user.
+      users.users.kai = {
+        isNormalUser = true;
+        home = "/home/kai";
+        description = "Kai";
+      };
+
+      # Use Home Manager to manage the user's dotfiles.
+      imports = [
+        <home-manager/nixos>
       ];
 
-    environment.systemPackages = with pkgs; [
-      # Flakes use Git to pull dependencies from data sources 
-      coreutils
-      git
-      vim
-      wget
-      curl
-    ];
-    # Set default editor to vim
-    environment.variables.EDITOR = "vim";
-
-    # Create a user.
-    users.users.kai = {
-      isNormalUser = true;
-      home = "/home/kai";
-      description = "Kai";
-    };
-
-    # Use Home Manager to manage the user's dotfiles.
-    imports = [
-      <home-manager/nixos>
-    ];
-
-    home.file.".config/nixpkgs/config.nix" = {
-      source = ./home.nix;
-      text = ''
-        { home.username }:
-        {
-          programs.home-manager.enable = true;
-        };
-      '';
-    };
-
-    # Enable the OpenSSH daemon.
-    services.openssh = {
-      enable = true;
-      settings = {
-        X11Forwarding = true;
-        PermitRootLogin = "no"; # disable root login
-        PasswordAuthentication = false; # disable password login
+      home.file.".config/nixpkgs/config.nix" = {
+        source = ./home.nix;
+        text = ''
+          { home.username }:
+          {
+            programs.home-manager.enable = true;
+          };
+        '';
       };
-      openFirewall = true;
+
+      # Enable the OpenSSH daemon.
+      services.openssh = {
+        enable = true;
+        settings = {
+          X11Forwarding = true;
+          PermitRootLogin = "no"; # disable root login
+          PasswordAuthentication = false; # disable password login
+        };
+        openFirewall = true;
+      };
     };
   };
 }
